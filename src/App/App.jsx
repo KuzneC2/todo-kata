9f64e7a32e5c8d8786f9f1d3c0ed8127c065bb6c
Dmitriy Kuznetsov <grebi.taban@mail.ru>
2024-10-17 23:54:54 +0300

    проверка husky

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,4 @@
-import React { Component } from 'react'
+import { Component } from 'react'
 import Footer from '../Footer/Footer'
 import NewTaskForm from '../NewTaskForm/NewTaskForm'
 import TaskList from '../TaskList/TaskList'
@@ -21,232 +21,231 @@ class App extends Component {
       editTaskDescription: ''
     };
   }
-  var a = 5
-
+ 
   updateTimer = () => {
     setInterval(() => {
-      this.setState(prevState => ({
-        dataTasks: prevState.dataTasks.map(task => ({
-          ...task,
-          timeCreated: `created ${this.timeCreate(task.timer)}`
-        })),
-        filteredDataTasks: prevState.filteredDataTasks.map(task => ({
-          ...task,
-          timeCreated: `created ${this.timeCreate(task.timer)}`
-        }))
-      }));
-    }, 15000);
-  }
+    this.setState(prevState => ({
+      dataTasks: prevState.dataTasks.map(task => ({
+        ...task,
+        timeCreated: `created ${this.timeCreate(task.timer)}`
+      })),
+      filteredDataTasks: prevState.filteredDataTasks.map(task => ({
+        ...task,
+        timeCreated: `created ${this.timeCreate(task.timer)}`
+      }))
+    }));
+  }, 15000);
+}
 
-  createTodoItem = (description, timer, status = '',) => {
-    return {
-      id: this.maxId++,
-      description,
-      timeCreated: `created ${this.timeCreate(timer)}`,
-      timer,
-      status,
-      defaulDescription: description
+createTodoItem = (description, timer, status = '',) => {
+  return {
+    id: this.maxId++,
+    description,
+    timeCreated: `created ${this.timeCreate(timer)}`,
+    timer,
+    status,
+    defaulDescription: description
+  }
+}
+
+
+
+timeCreate = (timer) => {
+  return formatDistanceToNowStrict(
+    new Date(timer),
+    {
+      addSuffix: true
     }
-  }
+  )
+}
 
+addItem = (description) => {
+  const newItem = this.createTodoItem(description, new Date() - 1);
+  this.setState(({ dataTasks, filteredDataTasks, filterName }) => {
+    const newArr = [newItem, ...dataTasks];
+    const newFilterArr = [newItem, ...filteredDataTasks]
 
-
-  timeCreate = (timer) => {
-    return formatDistanceToNowStrict(
-      new Date(timer),
-      {
-        addSuffix: true
+    if (filterName !== 'completed') {
+      return {
+        dataTasks: newArr,
+        filteredDataTasks: newFilterArr
       }
-    )
-  }
+    }
+    else {
+      return {
+        dataTasks: newArr,
+      };
+    }
+  });
+}
 
-  addItem = (description) => {
-    const newItem = this.createTodoItem(description, new Date() - 1);
-    this.setState(({ dataTasks, filteredDataTasks, filterName }) => {
-      const newArr = [newItem, ...dataTasks];
-      const newFilterArr = [newItem, ...filteredDataTasks]
+editTask = (id) => {
+  const newDescription = this.state.dataTasks.find(el => el.id == id)
+  this.setState({
+    filteredDataTasks: this.state.filteredDataTasks.map((task) => {
+      if (task.id == id) {
+        return { ...task, status: 'editing' }
+      }
+      if (task.status == 'editing') {
+        const prevStatus = this.state.dataTasks.find(el => el.id == task.id)
+        return { ...task, status: prevStatus.status }
+      }
+      else {
+        return task
+      }
+    }),
+    editTaskDescription: newDescription.description
+  })
+}
 
-      if (filterName !== 'completed') {
-        return {
-          dataTasks: newArr,
-          filteredDataTasks: newFilterArr
+editSubmit = (e, id) => {
+  e.preventDefault()
+  const updateDataObject = this.state.dataTasks.find(el => el.id === id)
+  this.setState((prevState) => ({
+    dataTasks: prevState.dataTasks.map(task => {
+      if (task.id === id) {
+        return { ...task, description: this.state.editTaskDescription, defaulDescription: this.state.editTaskDescription }
+      }
+      else {
+        return task
+      }
+    }),
+    filteredDataTasks: prevState.filteredDataTasks.map(task => {
+      if (task.id === id) {
+        return { ...task, description: this.state.editTaskDescription, status: updateDataObject.status, defaulDescription: this.state.editTaskDescription }
+      }
+      else {
+        return task
+      }
+    }),
+  }))
+
+}
+
+changeLabel = (e) => {
+  this.setState({
+    editTaskDescription: e.target.value
+  })
+
+}
+
+changeStatus = (id) => {
+  this.setState((prevState) => ({
+    dataTasks: prevState.dataTasks.map((task) => {
+      if (task.id == id) {
+        if (task.status == '') {
+          return { ...task, status: 'completed' }
+        }
+        else if (task.status == 'completed') {
+          return { ...task, status: '' }
         }
       }
       else {
-        return {
-          dataTasks: newArr,
-        };
+        return task
       }
-    });
-  }
-
-  editTask = (id) => {
-    const newDescription = this.state.dataTasks.find(el => el.id == id)
-    this.setState({
-      filteredDataTasks: this.state.filteredDataTasks.map((task) => {
-        if (task.id == id) {
-          return { ...task, status: 'editing' }
+    }),
+    filteredDataTasks: prevState.filteredDataTasks.map((task) => {
+      if (task.id == id) {
+        if (task.status == '') {
+          return { ...task, status: 'completed' }
         }
-        if (task.status == 'editing') {
-          const prevStatus = this.state.dataTasks.find(el => el.id == task.id)
-          return { ...task, status: prevStatus.status }
+        else if (task.status == 'completed') {
+          return { ...task, status: '' }
         }
-        else {
-          return task
-        }
-      }),
-      editTaskDescription: newDescription.description
-    })
-  }
-
-  editSubmit = (e, id) => {
-    e.preventDefault()
-    const updateDataObject = this.state.dataTasks.find(el => el.id === id)
-    this.setState((prevState) => ({
-      dataTasks: prevState.dataTasks.map(task => {
-        if (task.id === id) {
-          return { ...task, description: this.state.editTaskDescription, defaulDescription: this.state.editTaskDescription }
-        }
-        else {
-          return task
-        }
-      }),
-      filteredDataTasks: prevState.filteredDataTasks.map(task => {
-        if (task.id === id) {
-          return { ...task, description: this.state.editTaskDescription, status: updateDataObject.status, defaulDescription: this.state.editTaskDescription }
-        }
-        else {
-          return task
-        }
-      }),
-    }))
-
-  }
-
-  changeLabel = (e) => {
-    this.setState({
-      editTaskDescription: e.target.value
-    })
-
-  }
-
-  changeStatus = (id) => {
-    this.setState((prevState) => ({
-      dataTasks: prevState.dataTasks.map((task) => {
-        if (task.id == id) {
-          if (task.status == '') {
-            return { ...task, status: 'completed' }
-          }
-          else if (task.status == 'completed') {
-            return { ...task, status: '' }
-          }
-        }
-        else {
-          return task
-        }
-      }),
-      filteredDataTasks: prevState.filteredDataTasks.map((task) => {
-        if (task.id == id) {
-          if (task.status == '') {
-            return { ...task, status: 'completed' }
-          }
-          else if (task.status == 'completed') {
-            return { ...task, status: '' }
-          }
-        }
-        else {
-          return task
-        }
-      })
-    }))
-  };
-
-  deleteTask = (id) => {
-    this.setState(({ dataTasks, filteredDataTasks }) => {
-      const idx = dataTasks.findIndex(el => el.id === id);
-      const idxFilt = filteredDataTasks.findIndex(el => el.id === id)
-      return {
-        dataTasks: [...dataTasks.slice(0, idx), ...dataTasks.slice(idx + 1)],
-        filteredDataTasks: [...filteredDataTasks.slice(0, idxFilt), ...filteredDataTasks.slice(idxFilt + 1)]
+      }
+      else {
+        return task
       }
     })
-  }
+  }))
+};
 
-  componentDidMount() {
-    this.setState({ filteredDataTasks: this.state.dataTasks });
-    this.updateTimer()
+deleteTask = (id) => {
+  this.setState(({ dataTasks, filteredDataTasks }) => {
+    const idx = dataTasks.findIndex(el => el.id === id);
+    const idxFilt = filteredDataTasks.findIndex(el => el.id === id)
+    return {
+      dataTasks: [...dataTasks.slice(0, idx), ...dataTasks.slice(idx + 1)],
+      filteredDataTasks: [...filteredDataTasks.slice(0, idxFilt), ...filteredDataTasks.slice(idxFilt + 1)]
+    }
+  })
+}
 
-  }
+componentDidMount() {
+  this.setState({ filteredDataTasks: this.state.dataTasks });
+  this.updateTimer()
 
-
-
-
-  changeListAll = () => {
-    this.setState(() => ({
-      filteredDataTasks: this.state.dataTasks,
-      filterName: 'all'
-    }))
-
-  }
-
-  changeListActive = () => {
-    this.setState(() => ({
-      filteredDataTasks: this.state.dataTasks.filter(el => el.status === ''),
-      filterName: 'active'
-    }))
-  }
-
-  changeListComplete = () => {
-    this.setState(() => ({
-      filteredDataTasks: this.state.dataTasks.filter(el => el.status === 'completed'),
-      filterName: 'completed'
-    }))
-
-  }
-
-  clearCompleateItems = () => {
-    this.setState(() => ({
-      dataTasks: this.state.dataTasks.filter(el => el.status !== 'completed'),
-      filteredDataTasks: this.state.filteredDataTasks.filter(el => el.status !== 'completed')
-    }))
-  }
+}
 
 
-  render() {
-    const { filteredDataTasks } = this.state
-    const taskDone = this.state.dataTasks.filter(el => el.status === 'completed').length;
-    const taskLeft = this.state.dataTasks.length - taskDone;
 
-    return (
-      <>
-        <section className="todoapp">
-          <header className="header">
-            <h1>todos</h1>
-            <NewTaskForm
-              addItem={this.addItem}
-            />
-          </header>
-          <section className="main">
-            <TaskList
-              filteredDataTasks={filteredDataTasks}
-              toggleStatusTodo={this.changeStatus}
-              onDeleted={this.deleteTask}
-              onEdit={this.editTask}
-              editSubmit={this.editSubmit}
-              changeLabel={this.changeLabel}
-            />
-            <Footer
-              itemsLeft={taskLeft}
-              changeDirectoryAll={this.changeListAll}
-              changeDirectoryActive={this.changeListActive}
-              changeDirectoryComplete={this.changeListComplete}
-              clearCompleateItems={this.clearCompleateItems}
-            />
-          </section>
+
+changeListAll = () => {
+  this.setState(() => ({
+    filteredDataTasks: this.state.dataTasks,
+    filterName: 'all'
+  }))
+
+}
+
+changeListActive = () => {
+  this.setState(() => ({
+    filteredDataTasks: this.state.dataTasks.filter(el => el.status === ''),
+    filterName: 'active'
+  }))
+}
+
+changeListComplete = () => {
+  this.setState(() => ({
+    filteredDataTasks: this.state.dataTasks.filter(el => el.status === 'completed'),
+    filterName: 'completed'
+  }))
+
+}
+
+clearCompleateItems = () => {
+  this.setState(() => ({
+    dataTasks: this.state.dataTasks.filter(el => el.status !== 'completed'),
+    filteredDataTasks: this.state.filteredDataTasks.filter(el => el.status !== 'completed')
+  }))
+}
+
+
+render() {
+  const { filteredDataTasks } = this.state
+  const taskDone = this.state.dataTasks.filter(el => el.status === 'completed').length;
+  const taskLeft = this.state.dataTasks.length - taskDone;
+
+  return (
+    <>
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <NewTaskForm
+            addItem={this.addItem}
+          />
+        </header>
+        <section className="main">
+          <TaskList
+            filteredDataTasks={filteredDataTasks}
+            toggleStatusTodo={this.changeStatus}
+            onDeleted={this.deleteTask}
+            onEdit={this.editTask}
+            editSubmit={this.editSubmit}
+            changeLabel={this.changeLabel}
+          />
+          <Footer
+            itemsLeft={taskLeft}
+            changeDirectoryAll={this.changeListAll}
+            changeDirectoryActive={this.changeListActive}
+            changeDirectoryComplete={this.changeListComplete}
+            clearCompleateItems={this.clearCompleateItems}
+          />
         </section>
-      </>
-    )
-  }
+      </section>
+    </>
+  )
+}
 }
 
 
