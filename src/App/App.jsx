8dee56cefa858d5d86ace8f6c6436deaaac5743d
Dmitriy Kuznetsov <grebi.taban@mail.ru>
2024-10-17 23:09:40 +0300

    husky test1

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,10 @@
-import { Component } from 'react'
+import React { Component } from 'react'
 import Footer from '../Footer/Footer'
 import NewTaskForm from '../NewTaskForm/NewTaskForm'
 import TaskList from '../TaskList/TaskList'
 import { formatDistanceToNowStrict } from 'date-fns'
 import './App.css'
-asdasd
+
 class App extends Component {
   constructor() {
     super();
