ccfd14eef373e8997257118286adf10a3464feac
Dmitriy Kuznetsov <grebi.taban@mail.ru>
2024-10-17 22:39:25 +0300

    husky testing

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -38,7 +38,7 @@ class App extends Component {
     }, 15000);
   }
 
-  createTodoItem = (description, timer, status = '',) => {
+  createTodoItem = (description, timer, status = '', nt) => {
     return {
       id: this.maxId++,
       description,
